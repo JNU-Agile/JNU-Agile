f07d3968c836c2bea1862fdc125aad6a73842fbf
edisond <edisond@qq.com>
2014-04-30 22:02:52 +0800

    test

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            WebService web = new WebService();
+            Encoding encode = Encoding.UTF8;
+            String jsonText = web.GetWebContent("http://web.juhe.cn:8080/fund/netdata/all?key=927c4fed88698967319b811467f9b4c3", encode);
+            String result = String.Empty;
+            JsonReader reader = new JsonTextReader(new StringReader(jsonText));
+            String FILE_NAME = "MyFile.txt";
+            StreamWriter sr;
+            try
+            {
+                sr = File.CreateText(FILE_NAME);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                return;
+            }
+            while (reader.Read())
+            {
+                result += reader.Value;
+                result += " ";
+            }
+            sr.Write(result);
+            sr.Close();
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Web.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.IO;
+namespace WindowsFormsApplication1
+{
+    public class WebService
+    {
+        //获取整个网页
+        public String GetWebContent(String Url, Encoding encode)
+        {
+            try
+            {
+                if (Url.Equals("about:blank")) return null;
+                if (!Url.StartsWith("http://") && !Url.StartsWith("https://"))
+                {
+                    Url = "http://" + Url;
+                }
+
+
+                //根据获取网址向服务器发送请求：
+                HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(Url);
+                //设置超时(10秒)
+                myReq.Timeout = 60000;
+                myReq.Accept = "Accept-Language:zh-cn";
+                myReq.KeepAlive = true;
+                myReq.Referer = Url;
+                myReq.MaximumAutomaticRedirections = 100;
+                myReq.AllowAutoRedirect = true;
+                //网络响应请求
+                HttpWebResponse myres = (HttpWebResponse)myReq.GetResponse();
+                //获取网页字符流，并最终转换为系统默认的字符：
+                Stream WebStream = myres.GetResponseStream();
+                StreamReader Sreader = new StreamReader(WebStream, encode);
+                String HtmlText = Sreader.ReadToEnd();
+                //关闭字符流：
+                Sreader.Close();
+                WebStream.Close();
+                return HtmlText;
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
+                return null;
+            }
+        }
+
+    }
+}
