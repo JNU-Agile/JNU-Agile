a134ff0048bcc7a9cf498c717cf52f9b63bce369
zhengjingchun <877182978@qq.com>
2014-05-02 11:56:18 +0800

    test

---
+++ WindowsFormsApplication1/DBoperate.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication1
+{
+    class DBoperate
+    {
+        private readonly String HOSTSTR = "Server=.;Database=JNU-Agile;User ID=edisond;Password=33635468";
+        private SqlConnection dbConnection;
+        private SqlCommand cmd;
+        public void DatabaseConnector()
+	    {
+            dbConnection = new SqlConnection(HOSTSTR);
+	    }
+
+        //将今日数据添加进数据库
+        public bool AddFoudItem(List<Found> found)
+        {
+            try
+            {
+                dbConnection.Open();
+                foreach (Found foundItem in found)
+                { 
+                    cmd = new SqlCommand("insert into Table_Found values('"
+                        + foundItem.Code + "','"
+                        + foundItem.Name + "','"
+                        + foundItem.Newnet + "','"
+                        + foundItem.Totalnet + "','"
+                        + foundItem.Dayincrease + "','"
+                        + foundItem.Daygrowrate + "','"
+                        + foundItem.Weekgrowrate + "')"
+                        + foundItem.Annualincome + "')"
+                        + foundItem.Date + "')"
+                        , dbConnection);
+                    cmd.ExecuteNonQuery();
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.ToString());
+                return false;
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+        }
+
+        //根据日期查找基金
+        public Found GetFoundItem(DateTime date)
+        {
+            try
+            {
+                dbConnection.Open();
+                cmd = new SqlCommand("Select * from Table_Found where Date ='" + date.ToString() + "'", dbConnection);
+                SqlDataReader foundItem = cmd.ExecuteReader();
+                while (foundItem.Read())
+                {
+                    Found found = new Found();
+                    found.Code = foundItem.GetString(0);
+                    found.Name = foundItem.GetString(1);
+                    found.Newnet = foundItem.GetFloat(2);
+                    found.Totalnet = foundItem.GetFloat(3);
+                    found.Dayincrease = foundItem.GetFloat(4);
+                    found.Daygrowrate = foundItem.GetFloat(5);
+                    found.Weekgrowrate = foundItem.GetFloat(6);
+                    found.Annualincome = foundItem.GetFloat(7);
+                    return found;
+                }
+                return null;
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.ToString());
+                return null;
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Found.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
+
+namespace WindowsFormsApplication1
+{
+    //基金类
+    public class Found
+    {
+        private string m_code;
+        public string Code
+        {
+            get { return m_code; }
+            set { m_code = value; }
+
+        }
+
+        private string m_name;
+        public string Name
+        {
+            get { return m_name; }
+            set { m_name = value; }
+
+        }
+
+        private float m_newnet;
+        public float Newnet
+        {
+            get { return m_newnet; }
+            set { m_newnet = value; }
+        }
+
+        private float m_totalnet;
+        public float Totalnet
+        {
+            get { return m_totalnet; }
+            set { m_totalnet = value; }
+        }
+
+        private float m_dayincrease;
+        public float Dayincrease
+        {
+            get { return m_dayincrease; }
+            set { m_dayincrease = value; }
+        }
+
+        private float m_daygrowrate;
+        public float Daygrowrate
+        {
+            get { return m_daygrowrate; }
+            set { m_daygrowrate = value; }
+        }
+
+        private float m_weekgrowrate;
+        public float Weekgrowrate
+        {
+            get { return m_weekgrowrate; }
+            set { m_weekgrowrate = value; }
+        }
+        private float m_annualincome;
+        public float Annualincome
+        {
+            get { return m_annualincome; }
+            set { m_annualincome = value; }
+        }
+
+        //因为再本来的json中有日期，我就先加上了。
+        private DateTime m_date;
+        public DateTime Date
+        {
+            get { return m_date; }
+            set { m_date = value; }
+        }
+    }
+
+
+    //将json转化为对象
+    public class JsonTranslate
+    {
+        public List<Found> main (string url, Encoding code)
+        {
+            WebService web = new WebService();
+            string json = web.GetWebContent(url, code);
+            List<Found> todydata = JsonConvert.DeserializeObject<List<Found>>(json);
+            return todydata;
+         }
+     }
+}
