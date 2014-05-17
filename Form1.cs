99485dc0c55d6a234099a872cf9b0cc2efdab882
Chang5 <chang2249293078@gmail.com>
2014-05-17 16:44:30 +0800

    从网页下载基金代码和净值 Signed-off-by: Chang5 <chang2249293078@gmail.com>

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Windows.Forms;
 using Newtonsoft.Json;
 using System.IO;
+using System.Text.RegularExpressions;
+using System.Net;
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
@@ -16,7 +18,9 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
             WebService web = new WebService();
-            Encoding encode = Encoding.UTF8;
+            List<Fund> fundlist = web.getFundCode();
+            web.getNetValue(fundlist);
+            /*Encoding encode = Encoding.UTF8;
             String jsonText = web.GetWebContent("http://web.juhe.cn:8080/fund/netdata/all?key=927c4fed88698967319b811467f9b4c3", encode);
             String result = String.Empty;
             JsonReader reader = new JsonTextReader(new StringReader(jsonText));
@@ -37,7 +41,20 @@ namespace WindowsFormsApplication1
                 result += " ";
             }
             sr.Write(result);
-            sr.Close();
+            sr.Close();*/
+
+            
+
+
+
+
+
+
+
+            
+
+
+
         }
     }
 }
---
+++ NetValueOfFund.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GetNetValueOfFund
+{
+    class NetValueOfFund
+    {
+        public String date;
+        //float netValue;
+        public String netValue;
+        //float accumulativeNetValue;
+        public String earningRecently;
+        public String accumulativeNetValue;
+        //float dailyGrowthRate;
+        public String dailyGrowthRate;
+        public String stateOfPurse;
+        public String stateOfRedemption;
+        //float distribution;
+        public String earningPer7;
+        public String earningPer10000;
+        public String distribution;
+
+        public NetValueOfFund(String date, String netValue, String earningPer10000, String earningPer7, String earningRecently, String accumulativeNetValue, String dailyGrowthRate, String stateOfPurse, String stateOfRedemption, String distribution)
+        {
+            this.date = date;
+            this.netValue = netValue;
+            this.accumulativeNetValue = accumulativeNetValue;
+            this.earningPer10000 = earningPer10000;
+            this.earningPer7 = earningPer7;
+            this.earningRecently = earningRecently;
+            this.dailyGrowthRate = dailyGrowthRate;
+            this.stateOfPurse = stateOfPurse;
+            this.stateOfRedemption = stateOfRedemption;
+            this.distribution = distribution;
+        }
+    }
+
+}
